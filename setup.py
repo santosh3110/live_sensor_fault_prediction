bafb0f8ed48ad1511bd6456a08643df2dde11585
Santosh Kumar Guntupalli <santoshkumarguntupalli@gmail.com>
2025-02-19 13:53:30 +0000

    setup.py and requirements.txt created

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphen_e_dot ="-e ."
+
+def get_requirements(file_path:str)->list[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n","") for req in requirements]
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+
+setup(
+    name="live_sensor_fault_prediction",
+    version="0.0.1",
+    author="Santosh Kumar Guntupalli",
+    author_email="santoshkumarguntupalli@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
